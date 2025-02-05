0c8affbc79db73ffccd4199d0238e406f7d3e550
Muhammed753209 <157942384+TitaniumFeather@users.noreply.github.com>
2025-02-05 14:22:28 -0500

    Update U1Lesson2.py

--- U1Lesson2.py
+++ U1Lesson2.py
@@ -1,1 +1,34 @@
+#original code:
+print(24)
 
+# Predict: 
+# I predict that the number 24 will be printed to the console.
+
+# Run:
+# My prediction was correct! The output was indeed the number 24.
+
+# Modify:
+amount = 24
+print("Two dozen is: ", amount)
+# Reflection:
+# Including "Two dozen is: " in the the code allows the code to have a "goal" in a way, in oppesed to a random number. Additionaly it also makes the code more readable to the user.
+
+#Modify 2:
+amount = 7*4
+print("twenty-eight is: ", amount)
+
+#Modify 3:
+amount = 8
+amount = amount + 1
+print("The amount is: ", amount)
+#Reflection:
+#After running the code, "amount = amount + 1" increments the value of "amount" by one and asigns this new value to amoutn. 
+#"amount += 1" is a more compact way, but achieves the same thing.
+
+#Modify 3:
+amount = 8
+amount = amount * 2
+print(amount)
+#Reflection:
+# amount = amount * 2 is taking the value of amount and multiplying it by two, it doubles the value of amount and assigns that as the new value of amount.
+# amount *= 2 works the same way but just more compact.
