531293aedc7e805f5dce0c7083daf7e70f8771d1
Muhammed753209 <157942384+TitaniumFeather@users.noreply.github.com>
2025-02-07 14:25:51 -0500

    U1Lesson5.py

--- U1Lesson5.py
+++ U1Lesson5.py
@@ -4,3 +4,21 @@
   #length = float(length)
   #area = math.pow(length, 2)
   #print("The area of a square of side length", length, "is:", area)
+
+#Predict: 
+#I believe that the code above will print the following:
+  #Please input a length:
+#It will take this input and assign it to the "length" variable and convert the variable to a float. The code will then creat an "area" varaible by putting "length" to the power of 2 and will return the following:
+#The area of a square of side length", length, "is:", area
+
+# Run
+# My prediction was correct
+
+# Inspect 
+# The program stopped execution after the first print statement since it will wait for the user to input a number 
+# I inputted the number 2 and it outputted 4.0 which matches with my calculator 
+
+#Modify:
+
+
+  
