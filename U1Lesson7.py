cde7e6645c838174aa89e20cb0cf69adce75ba63
freakbob2993423 <157942384+TitaniumFeather@users.noreply.github.com>
2025-02-10 12:54:34 -0500

    U1Lesson7.py

---
+++ U1Lesson7.py
@@ -0,0 +1,17 @@
+#given code:
+#import math
+#x = input("Please input a whole number: ")
+#x = int(x)
+#y = input("Please input another nonzero whole number")
+#y = int(y)
+#print("Now deciding if", y, "is a factor of", x, "...")
+#rem = x % y
+#if rem == 0:
+    #print("Yes!", y, "is a factor of", x)
+
+#Predict:
+#Code will ask user for whole number
+#Code will then conver tthe string input to integer
+#Code will ask for another non zero number and convert it to int
+#Code will log the text "Now deciding if", y, "is a factor of", x, "..."
+#If remainder between two numbers is 0, code will print "Yes!", y, "is a factor of", x
