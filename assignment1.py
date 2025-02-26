e0698f8a50d0f0dd19eafa45a29e2d58c1cde98b
freakbob2993423 <157942384+TitaniumFeather@users.noreply.github.com>
2025-02-26 12:56:48 -0500

    assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,4 @@
+import math
 change_original = int(input("Please enter the amount of change in cents: "))
 change = change_original
 
@@ -5,58 +6,27 @@ amount_quarters = 0
 amount_dimes = 0
 amount_nickels = 0
 amount_pennies = 0
+amount_dollars = 0
 
 if change >= 100:
-  change -= 100
+  amount_dollars = math.trunc(change/100)
+  change = change % 100
 
-if change >= 200:
-  change -= 200
-
-if change >= 300:
-  change -= 300
+if change > 25:
+  amount_quarters = math.trunc(change/25)
+  change = change % 25
   
-if change >= 400:
-  change -= 400
-
-if change >= 75:
-  change -= 75
-  amount_quarters += 3
+if 10 <= change < 25:
+  amount_dimes = math.trunc(change/10)
+  change = change % 10
   
-if 50 <= change < 75:
-  change -= 50
-  amount_quarters += 2
-
-if 25 <= change < 50:
-  change -= 25
-  amount_quarters += 1
-
-if 15 <= change < 25:
-  change -= 10
-  amount_dimes += 1
-
-if 10 <= change < 15:
-  change -= 10
-  amount_dimes += 1
-
 if 5 <= change < 10:
-  change -= 5
-  amount_nickels +=1
+  amount_nickels = math.trunc(change/5)
+  change = change % 5
 
-if change == 4:
-  change -= 4
-  amount_pennies += 4
-
-if change == 3:
-  change -= 3
-  amount_pennies += 3
-
-if change == 2:
-  change -= 2
-  amount_pennies += 2
-
-if change == 1:
-  change -= 1
-  amount_pennies += 1
+if 1<= change < 5:
+  amount_pennies = math.trunc(change/1)
+  change = change % 1
 
 if amount_quarters == 1:
   quarter_text = "quarter"
@@ -89,6 +59,14 @@ elif amount_nickels > 1:
 else:
   nickel_text= ""
   amount_nickels = ""
+  
+if amount_dollars == 1:
+  dollar_text = "dollar"
+elif amount_dollars > 1:
+  dollar_text = "dollars"
+else:
+  dollar_text= ""
+  amount_dollars = ""
 
 if change == 0:
-  print(f"{change_original} cents can be {amount_quarters} {quarter_text} {amount_dimes} {dime_text} {amount_nickels} {nickel_text} {amount_pennies} {penny_text}.")
+  print(f"{change_original} cents can be {amount_dollars} {dollar_text} {amount_quarters} {quarter_text} {amount_dimes} {dime_text} {amount_nickels} {nickel_text} {amount_pennies} {penny_text}.")
