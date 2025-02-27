695641da2e1f02003b84167362c01fdea908dc99
Muhammed753209 <157942384+TitaniumFeather@users.noreply.github.com>
2025-02-27 14:08:08 -0500

    assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,4 +1,3 @@
-import math
 change_original = int(input("Please enter the amount of change in cents: "))
 change = change_original
 
@@ -15,64 +14,53 @@ if change >= 100:
   change = change - (change - (change % 100))
 
 if change >= 25:
-  amount_quarters = math.trunc(change/25)
+  amount_quarters = int(change/25)
   change = change % 25
   
 if 10 <= change < 25:
-  amount_dimes = math.trunc(change/10)
+  amount_dimes = int(change/10)
   change = change % 10
   
 if 5 <= change < 10:
-  amount_nickels = math.trunc(change/5)
+  amount_nickels = int(change/5)
   change = change % 5
 
 if 1<= change < 5:
-  amount_pennies = math.trunc(change/1)
+  amount_pennies = int(change/1)
   change = change % 1
 
 if amount_quarters == 1:
   quarter_text = str(amount_quarters) + " quarter,"
-  list.append(quarter_text)
 elif amount_quarters > 1:
   quarter_text = str(amount_quarters) + " quarters,"
-  list.append(quarter_text)
 else:
   quarter_text= ""
   amount_quarters =""
 
 if amount_dimes == 1:
   dime_text = str(amount_dimes) + " dime,"
-  list.append(dime_text)
 elif amount_dimes > 1:
   dime_text = str(amount_dimes) + " dimes,"
-  list.append(dime_text)
 else:
   dime_text= ""
   amount_dimes = ""
 
 if amount_pennies == 1:
-  penny_text = str(amount_pennies) + " penny"
-  list.append(penny_text)
+  penny_text = str(amount_pennies) + " penny,"
 elif amount_pennies > 1:
-  penny_text = str(amount_pennies) + " pennies"
-  list.append(penny_text)
+  penny_text = str(amount_pennies) + " pennies,"
 else:
   penny_text= ""
   amount_pennies = ""
 
 if amount_nickels == 1:
   nickel_text = str(amount_nickels) + " nickel,"
-  list.append(nickel_text)
 elif amount_nickels > 1:
   nickel_text = str(amount_nickels) + " nickels,"
-  list.append(nickel_text)
 else:
   nickel_text= ""
   amount_nickels = ""
-
-list.insert(-1, "and")
-list[-1] = list[-1].replace(',', '')
-joinedList = ' '.join(list)
+  
 
 if change == 0:
-  print(f"{change_original} cents can be {joinedList}.")
+  print(f"{change_original} cents can be {quarter_text} {dime_text} {nickel_text} {penny_text}.")
