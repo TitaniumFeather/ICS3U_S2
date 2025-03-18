996c358b92864391bdc18a45c64fdef07327d5c4
Muhammed753209 <157942384+TitaniumFeather@users.noreply.github.com>
2025-03-17 22:23:08 -0400

    assignment 2.py

--- assignment 2.py
+++ assignment 2.py
@@ -18,11 +18,13 @@ import random
 # Prints a welcome message to the user.
 print("Hello! Welcome to the number guessing game!")
 # Tells the the user about the game and how it functions.
-print("I am thinking of a number between 1 and 100. It is your turn to guess what it is.You have a maximum of six (6) tries.")
+print("I am thinking of a number between 1 and 100. It is your turn to guess what it is.")
+# Tells user how many tries they have to guess.
+print("You have a maximum of six (6) tries.")
 
-# Generates a random number between 1 and 100 (inclusive of 1, exclusive of 100).
-num = random.randrange(1, 100, 1) 
-# Creates the variable "guess" representing number of guesses) and assigns it to zero.
+# Generates a random number between 1 and 100.
+num = random.randint(1, 100) 
+# Creates the variable "guess" (representing number of guesses) and assigns it to zero.
 guess = 0
 
 # Loops until the user has guessed 6 times.
@@ -31,25 +33,25 @@ while guess < 6:
   guess += 1  
   # Asks the user for input, converts it to an integer, and stores it in guess_num (represents the number user guessed).
   guess_num = int(input("Guess #%d: " % guess))  
-  # Ff the guessed number is equal to the randomly generated number, indented code will run.
+  # If the guessed number is equal to the randomly generated number, code below will run.
+   
   if guess_num == num:
     # Tells the user they guessed correctly.
     print("You guessed right!")  
     # Exits the loop since the correct number was guessed.
     guess = 7  
-  # If the guess is incorrect and there are remaining attempts:
+  # If the guess is incorrect and there are remaining attempts, code below will run.
   elif guess < 6:  
     # If the guess is too high:
     if guess_num > num:  
-      # Instructs the user to guess a lower number.
+      # Tells the user to guess a lower number.
       print("Lower!")  
     # If the guess is too low:
     else: 
-      # Instructs the user to guess a higher number.
+      # Tells the user to guess a higher number.
       print("Higher!")  
 
-# If the loop ends and the user hasn't guessed correctly:
+# If the loop was over and the user didn't correctly the code below will run.
 if guess_num != num:
-  # Informs the user they are out of guesses and reveals the correct number.
+  # Tells the user they are out of guesses and tells them the correct number.
   print("Sorry: you are out of guesses! The answer was %d. Better luck next time!" % num)
-
