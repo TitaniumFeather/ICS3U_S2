4d4f2c6d95355743ea7e2c993d55bd6690e19394
Muhammed753209 <157942384+TitaniumFeather@users.noreply.github.com>
2025-03-25 13:49:01 -0400

    U3Lesson1.py

---
+++ U3Lesson1.py
@@ -0,0 +1,25 @@
+# Prediciton A:
+# Python
+
+# Prediction B:
+# P
+
+# Prediction C:
+# Will print "Python on the same line"
+
+# Predcition D:
+# The point of end= is to make sure each letter is on the same line instead of a new one.
+
+# Prediction E:
+# The point of the empty print staatement is to create separation between the two loops.
+
+# Prediction F:
+# Will print numbers 0 - 5 with "Python with each letter and number on a new line.
+
+progname = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in."
+spaces = 0
+for i in progname:
+  if i == " ":
+    spaces += 1
+
+print("There are %d spaces in the text." % spaces)
