f9d99858340c0c05163827d3461bc07b8c597966
Muhammed753209 <157942384+TitaniumFeather@users.noreply.github.com>
2025-04-03 17:12:03 -0400

    U3Lesson4.py

--- U3Lesson4.py
+++ U3Lesson4.py
@@ -1,1 +1,14 @@
+# Modify
+ar2 = [[3, 4, 1, 2, 6],
+       [9, 2, 3, 7, 5],
+       [4, 2, 1, 0, 3]]
+one = 0
+empty = []
 
+for j in range(3):
+  for i in range(5):
+    one = one + ar2[j][i]
+  empty.append(one)
+  one = 0
+
+print(empty)
