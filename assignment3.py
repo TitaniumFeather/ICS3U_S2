8d23e226b9529114951fa143de7c32a0d79bdc34
Muhammed753209 <157942384+TitaniumFeather@users.noreply.github.com>
2025-04-07 19:17:16 -0400

    assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,18 +1,21 @@
-print("Palindrom program!")
-palindromeArray = ["civic", "basketball", "radar", "level", "kayak", "soccer", "racecar", "madam", "refer", "lool"]
-
-counter = 0
+print("Palindrome program!")
+palindromeArray = ["civic", "basketball", "radar", "goal", "kayak", "soccer", "racecar", "hotel", "refer", "loop"]
 
 for word in palindromeArray:
-  maxNum = len(word)//2
-  for j in range(maxNum):
-    if word[j] == word[len(word)-1-j]:
-      counter +=1
+  letterMatches = 0
+  letterPosition = 0
+  halfway = len(word) // 2
+  while letterPosition < halfway:
+    if word[letterPosition] == word[len(word) - 1 - letterPosition]:
+      letterMatches += 1
     else:
       print(word, "is not a palindrome!" )
-      break
-    if counter == maxNum:
+      letterPosition = halfway
+    if letterMatches == halfway:
       print(word, "is a palindrome!" )
+    letterPosition += 1
+    
+print("Goodbye!")
      
   
 
