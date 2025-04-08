5b5a47965eb04810f1ab5642924a1dfd4592a7ac
Muhammed753209 <157942384+TitaniumFeather@users.noreply.github.com>
2025-04-08 13:05:28 -0400

    assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,7 +1,11 @@
+# Informs user about the program details
 print("Palindrome program!")
+# Creates an array with words either palindrome 
 palindromeArray = ["civic", "basketball", "radar", "goal", "kayak", "soccer", "racecar", "hotel", "refer", "loop"]
 
+# Creates for loop that cycles through each word in the palindromeArray
 for word in palindromeArray:
+  # Sets 
   letterMatches = 0
   letterPosition = 0
   halfway = len(word) // 2
@@ -9,10 +13,10 @@ for word in palindromeArray:
     if word[letterPosition] == word[len(word) - 1 - letterPosition]:
       letterMatches += 1
     else:
-      print(word, "is not a palindrome!" )
+      print(word, "is not a palindrome")
       letterPosition = halfway
     if letterMatches == halfway:
-      print(word, "is a palindrome!" )
+      print(word, "is a palindrome")
     letterPosition += 1
     
 print("Goodbye!")
