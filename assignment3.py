e1a347efd4b24823aaeafe05ee9adf5f42cb1506
Muhammed753209 <157942384+TitaniumFeather@users.noreply.github.com>
2025-04-15 14:25:35 -0400

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -9,15 +9,18 @@ for word in palindromeArray:
   letterMatches = 0
   letterPosition = 0
   halfway = len(word) // 2
+  
   while letterPosition < halfway:
+    
     if word[letterPosition] == word[len(word) - 1 - letterPosition]:
       letterMatches += 1
+      letterPosition += 1
     else:
       print(word, "is not a palindrome")
       letterPosition = halfway
+      
     if letterMatches == halfway:
       print(word, "is a palindrome")
-    letterPosition += 1
 
 # 
 print("Goodbye!")
