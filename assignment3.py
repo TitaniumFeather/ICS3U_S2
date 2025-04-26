0ab71c062572b2b5ea45d0107d6bedac5798bd7a
Muhammed753209 <157942384+TitaniumFeather@users.noreply.github.com>
2025-04-25 23:21:26 -0400

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -20,25 +20,33 @@ palindromeArray = ["civic", "basketball", "radar", "goal", "kayak", "soccer", "r
 
 # Creates for loop that cycles through each word in the palindromeArray
 for word in palindromeArray:
-  # Creates variable which tracks the amount of matches between letters
+  # Creates variable which tracks the amount of matches between a letter and it's mirroring letter (set to 0)
   letterMatches = 0
+  # Create variable which tracks the index of a letter in chsoen word (set to 0)
   letterPosition = 0
+  # Creates variable which represents halway of chosen word
   halfway = len(word) // 2
   
+  # Loops through the first half of the word
   while letterPosition < halfway:
-    
+     
+    # Compares the letter at the current position with its mirrored letter from the end
     if word[letterPosition] == word[len(word) - 1 - letterPosition]:
+      # If letters match, increment the match count
       letterMatches += 1
+      # Move to the next letter position
       letterPosition += 1
+    # If letters don't match:
     else:
+      # print word is not palindrome
       print(word, "is not a palindrome")
+      # Exits the loop
       letterPosition = halfway
       
+    # If all mirrored letter pairs matched:
     if letterMatches == halfway:
+      # print the word is a palindrome
       print(word, "is a palindrome")
 
-# 
+# Informs user that the program has ended
 print("Goodbye!")
-     
-  
-
