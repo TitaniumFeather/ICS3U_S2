cc9784ec0287dd3c3ac2155a7fa6603edb9912f3
Muhammed753209 <157942384+TitaniumFeather@users.noreply.github.com>
2025-04-29 14:00:31 -0400

    U3Lesson6.py

--- U3Lesson6.py
+++ U3Lesson6.py
@@ -1,1 +1,49 @@
+# Make:
+def factorize(n):
+  
+  sum_num = 0
+  arr = []
+  
+  for i in range(1, n):
+    if n % i == 0:
+      arr.append(i)
+  
+  return arr
 
+# Make more
+arr = factorize(n)
+
+def total(arr):
+  sum_num = 0
+  for number in arr:
+    sum_num += number
+  
+  return sum_num
+
+print(total(arr))
+
+# Optional "assignment form:
+n = int(input("Please enter a number"))
+
+def factorize(n):
+  
+  sum_num = 0
+  arr = []
+  
+  for i in range(1, n):
+    if n % i == 0:
+      arr.append(i)
+  
+  for number in arr:
+    sum_num += number
+  
+  print("Factor sum:", sum_num)
+  
+  if sum_num == n:
+    print(n, "is perfect")
+  elif sum_num < n:
+    print(n, "is deficient")
+  else:
+    print(n, "is abundant")
+    
+print(factorize(n))
