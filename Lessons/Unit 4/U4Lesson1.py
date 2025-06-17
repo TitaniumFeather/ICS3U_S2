e66d9947555be78fd8c05ea419b4ba9a3716dca1
Muhammed753209 <157942384+TitaniumFeather@users.noreply.github.com>
2025-06-17 14:36:03 -0400

    Update U4Lesson1.py

--- Lessons/Unit 4/U4Lesson1.py
+++ Lessons/Unit 4/U4Lesson1.py
@@ -1,1 +1,18 @@
-fsdg
+# I think the output will be [-1, 0, 2, 3, 4, 7, 9, 11, 14]
+
+# Modify
+
+def swap(B, p, q):
+    temp = B[p]
+    B[p] = B[q]
+    B[q] = temp
+
+def sort(C): # A very inefficient sorting method!
+    for i in range(len(C)):
+        for j in range(len(C)):
+            if (C[i] < C[j]):
+                swap(C, i, j)
+
+A = [4, -1, 7, 3, 9, 0, 11, 2, 14]
+sort(A)
+print(A)
